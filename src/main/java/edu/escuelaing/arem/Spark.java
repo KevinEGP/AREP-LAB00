ce9d6c2097139430e951a886e64e124158745a55
Kevin Garzón <60078276+KevinEGP@users.noreply.github.com>
2022-01-26 14:02:02 -0500

    Se añadió clase principal

--- src/main/java/edu/escuelaing/arem/Spark.java
+++ src/main/java/edu/escuelaing/arem/Spark.java
@@ -1,13 +1,12 @@
 package edu.escuelaing.arem;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import static spark.Spark.*;
+
+public class Spark 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hello", (req, res) -> "Hello Heroku");
     }
 }
